b8597f8d4d2ded0fa6673d370ba9fc81104533a2
jgagnon <joseph.gagnon@beast-code.com>
2026-01-20 10:36:12 -0600

    A new error condition.

--- src/components/App.js
+++ src/components/App.js
@@ -94,7 +94,7 @@ class App extends Component {
 
   checkout() {
     // Generate an error
-    this.myCodeIsMorePerfect();
+    this.myCodeIsEvenBetter();
 
     const order = {
       email: this.email,
